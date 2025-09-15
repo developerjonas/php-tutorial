3335c440cb4a50f1e2b907ed51b5b7337f90a5dc
developerjonas <thedeveloperjonas@gmail.com>
2025-09-15 18:12:06 +0545

    Merged

---
+++ #02/index.php
@@ -0,0 +1,27 @@
+<!-- // Variables are the data storers  -->
+<?php
+$name = "php";
+echo "Our favuorite language is". $name;
+echo "<br>";
+echo $name ." is best";
+echo "<br>";
+
+$num = 123;
+echo $num;echo "<br>";
+
+$this_is_a_good_name = "php";
+echo $this_is_a_good_name;
+echo "<br>";
+
+$variable = " a variable ";echo "<br>";
+
+$VARIABLE = " another variable ";echo "<br>";
+
+$VariablE = " another another variable ";echo "<br>";
+
+
+echo $variable . $VARIABLE . $VariablE;
+echo "<br>";
+
+ECHO "This is a php tutorial";
+?>
---
+++ #03/index.php
@@ -0,0 +1,56 @@
+<!-- // What are operators
+
+Operators help us to perform various operations on variables and other datatypes -->
+
+<?php
+
+// Assignment Oprator
+
+// $a = 10;
+// $b = $a;
+// echo $b;
+
+// Arithmetic Operators
+
+$a = 10;
+$b = 15;
+
+echo $a;
+echo '<br>';
+echo $b;
+
+echo "The output of ther operation a * b is". $a * $b;
+echo '<br>';
+
+echo "The output of ther operation a + b is". $a + $b;
+echo '<br>';
+
+echo "The output of ther operation a - b is". $a - $b;
+echo '<br>';
+
+echo "The output of ther operation a / b is". $a / $b;
+echo '<br>';
+echo '<br>';
+
+// Comparision Operators
+
+$c = 30;
+$d = 15;
+
+// echo $c >= $d;
+// echo '<br>';
+
+// echo $c >= $d;
+// echo '<br>';
+
+// echo $c == $d;
+// echo '<br>';
+
+// echo $c != $d;
+// echo '<br>';
+
+// echo $c < $d;
+// echo '<br>';
+
+// echo $c > $d;
+// echo '<br>';
