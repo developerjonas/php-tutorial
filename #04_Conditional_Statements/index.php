4e3223fe1c20da6064e63ba4a527e9ab384f42da
developerjonas <thedeveloperjonas@gmail.com>
2025-09-15 18:07:11 +0545

    Initial Commit

---
+++ #04_Conditional_Statements/index.php
@@ -0,0 +1,16 @@
+<!-- CONDIOTIONAL STATEMENTSA IN PHP -->
+
+<?php
+
+echo "Hello World";
+
+$age = 17;
+
+if($age == 18){
+    echo" YES YOU CAN DRIVE ";
+} elseif ($age > 18){
+    echo" YES YOU CAN DRIVE  ";
+} else {
+    echo " SORRY U CANNOT ";
+}
+
