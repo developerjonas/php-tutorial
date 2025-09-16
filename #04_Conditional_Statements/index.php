bc1ec71594c2f8d1c08b2f39cb35a631adb32502
developerjonas <thedeveloperjonas@gmail.com>
2025-09-16 08:24:14 +0545

    Upload

--- #04_Conditional_Statements/index.php
+++ #04_Conditional_Statements/index.php
@@ -1,10 +1,10 @@
-<!-- CONDIOTIONAL STATEMENTSA IN PHP -->
+<!-- CONDITIONAL STATEMENTS IN PHP -->
 
 <?php
 
-echo "Hello World";
+echo "Hello World"."<br>";
 
-$age = 17;
+$age = 20;
 
 if($age == 18){
     echo" YES YOU CAN DRIVE ";
@@ -14,3 +14,82 @@ if($age == 18){
     echo " SORRY U CANNOT ";
 }
 
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+if($age == 17){
+    echo "The condition was satisfied"."<br>";
+} else {
+    echo "Hello  Friends";
+}
+
+echo "Hello";
+
+
+
+
+
+echo "This alwasy prints";
+
+
+
+
+
+$day = "Monday";
+
+if ($day == "Sunday"){
+    echo "1st Day";
+} else if ($day == "Monday"){
+    echo "2nd Day";
+} else if ($day == "Tuesday"){
+    echo "3rd Day";
+} else if ($day == "Wednesday"){
+    echo "4th Day";
+}
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+// CASE SWITCH STATEMENTS
+
+
+$day = "njaklsdsa";
+
+switch($day){
+    case "Monday":
+        echo "2nd day" ;
+        break;
+    case "Tuesday":
+        echo "3rd Day";
+        break;
+    case "Wednesday":
+        echo "4th Day";
+        break;
+    case "Thursday":
+        echo"5th Day";
+        break;
+    case "Friday":
+        echo "6th Day";
+        break;
+    default:
+        echo"Its must be sunday";
+}
+
+
+$age = 17;
+$hasID = false;
+
+if($age >=18){
+    if($hasID){
+        echo"We can enter the club";
+    } else {
+        echo "U need to make your ID U are old enough";
+    }
+} else {
+    echo "You are not old enough";
+}
+
+//
